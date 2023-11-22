7a5a0629ae6499c21cd24d7c74002e2caa12b5ce
Alex M <aleksandrsmusaluks@gmail.com>
2023-11-22 22:07:49 +0200

    Refactoring, wrapper object added, better naming for variables

--- main.py
+++ main.py
@@ -1,7 +1,49 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
 import telebot
 import os
+
+
+class DiscountWrapper:
+    title: ''
+    old_price: 0
+    new_price: 0
+    paldies_price: 0
+    percent: 0
+    image_url: ''
+    date: ''
+
+    def __init__(self):
+        self.title = None
+        self.old_price = None
+        self.new_price = None
+        self.paldies_price = None
+        self.percent = None
+        self.image_url = None
+        self.date = None
+
+    def set_title(self, title):
+        self.title = title
+
+    def set_old_price(self, old_price):
+        self.old_price = old_price
+
+    def set_new_price(self, new_price):
+        self.new_price = new_price
+
+    def set_paldies_price(self, paldies_price):
+        self.paldies_price = paldies_price
+
+    def set_percent(self, percent):
+        self.percent = percent
+
+    def set_image_url(self, image_url):
+        self.image_url = image_url
+
+    def set_date(self, date):
+        self.date = date
 
 
 def maxima_search(search_thing):
@@ -17,62 +59,71 @@ def scrap_data(data):
     results = []
     for dat in data:
         # print(dat)
-        result = ''
+        result = DiscountWrapper()
+
+        img_tag = dat.find('div', class_='img').find('img')
+        src_value = img_tag.get('src')
+        result.set_image_url(src_value)
+
         bottom = dat.find("div", class_="bottom-icon")
         paldies = dat.find("div", class_='t1 paldies-card')
+
         percent_wrapper = dat.find("div", class_='percents_wrapper')
         if bottom:
-            obj = get_percent_spans(bottom)
-            result = result + obj + '\n'
+            percent = get_percent_spans(bottom)
+            result.set_percent(percent)
         elif paldies:
             paldies_spans = paldies.find_all('span')
-            paldies_obj = ''
-            for span in paldies_spans:
-                classes = span.get('class')
-                if 'value' in classes:
-                    paldies_obj = span.text
-                elif 'cents' in classes:
-                    paldies_obj = paldies_obj + ',' + span.text
-                elif 'eur' in classes:
-                    paldies_obj = paldies_obj + span.text
-            result = result + paldies_obj + '\n'
+            paldies_price = ''
+            for paldies_span in paldies_spans:
+                paldies_class = paldies_span.get('class')
+                if 'value' in paldies_class:
+                    paldies_price = paldies_span.text
+                elif 'cents' in paldies_class:
+                    paldies_price = paldies_price + ',' + paldies_span.text
+                elif 'eur' in paldies_class:
+                    paldies_price = paldies_price + paldies_span.text
+            result.set_paldies_price(paldies_price)
         elif percent_wrapper:
-            obj = get_percent_spans(percent_wrapper)
-            result = result + obj + '\n'
-        t1 = dat.find("div", class_="t1")
-        if t1:
-            spans = t1.find_all("span")
-            t1_obj = ''
-            for span in spans:
-                classes = span.get("class")
-                if 'value' in classes:
-                    t1_obj = span.text
-                elif 'cents' in classes:
-                    t1_obj = t1_obj + ',' + span.text
-                elif 'eur' in classes:
-                    t1_obj = t1_obj + span.text
-            result = result + t1_obj + '\n'
-        t3 = dat.find("div", class_="t3")
-        if t3:
-            spans = t3.find_all("span")
-            t1_obj = ''
-            for span in spans:
-                classes = span.get("class")
-                if 'value' in classes:
-                    t1_obj = span.text
-                elif 'cents' in classes:
-                    t1_obj = t1_obj + ',' + span.text
-                elif 'eur' in classes:
-                    t1_obj = t1_obj + span.text
-            result = result + t1_obj + '\n'
+            percent = get_percent_spans(percent_wrapper)
+            result.set_percent(percent)
 
-        title = dat.find("div", class_='title')
-        result = result + title.text + '\n'
+        new_price_div = dat.find("div", class_="t1")
+        if new_price_div:
+            new_price_spans = new_price_div.find_all("span")
+            new_price = ''
+            for new_price_span in new_price_spans:
+                paldies_class = new_price_span.get("class")
+                if 'value' in paldies_class:
+                    new_price = new_price_span.text
+                elif 'cents' in paldies_class:
+                    new_price = new_price + ',' + new_price_span.text
+                elif 'eur' in paldies_class:
+                    new_price = new_price + new_price_span.text
+            result.set_new_price(new_price)
+
+        old_price_div = dat.find("div", class_="t3")
+        if old_price_div:
+            old_price_spans = old_price_div.find_all("span")
+            old_price = ''
+            for old_price_span in old_price_spans:
+                old_price_class = old_price_span.get("class")
+                if 'value' in old_price_class:
+                    old_price = old_price_span.text
+                elif 'cents' in old_price_class:
+                    old_price = old_price + ',' + old_price_span.text
+                elif 'eur' in old_price_class:
+                    old_price = old_price + old_price_span.text
+            result.set_old_price(old_price)
+
+        title_div = dat.find("div", class_='title')
+        result.set_title(title_div.text)
+
         date_interval_divs = dat.find_all("div", attrs={"data-dates-interval": True})
         if date_interval_divs:
-            for div in date_interval_divs:
-                date_interval = div["data-dates-interval"]
-                result = result + date_interval + '\n'
+            for date_div in date_interval_divs:
+                date_interval = date_div["data-dates-interval"]
+                result.set_date(date_interval)
         results.append(result)
     return results
 
@@ -113,7 +164,22 @@ def search(message):
         bot.send_message(message.chat.id, 'Nothing found')
     else:
         for result in results:
-            bot.send_message(message.chat.id, result)
+            maxima_prefix = 'https://www.maxima.lv/'
+            img_url = maxima_prefix + result.image_url
+            cleaned_url = re.sub(r'\.png.*$', '.png', img_url)
+            bot.send_photo(message.chat.id, cleaned_url)
+
+            formatted_message = ''
+            if result.old_price is not None:
+                formatted_message = formatted_message + f'<strike>{result.old_price}</strike>\n'
+            if result.new_price is not None:
+                formatted_message = formatted_message + f'<b>{result.new_price}</b>\n\n'
+            if result.title is not None:
+                formatted_message = formatted_message + f'{result.title}'
+            if result.date is not None:
+                formatted_message = formatted_message + f'\n\n<em>{result.date}</em>'
+
+            bot.send_message(message.chat.id, formatted_message, parse_mode='html')
 
 
 bot.infinity_polling()
